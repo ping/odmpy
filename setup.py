7592dd3732e1c8ee9e50b42e1cb51df4b29f7859
ping <ping@users.noreply.github.com>
2023-02-27 05:40:44 +0800

    Bump version to 0.7.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup  # type: ignore[import]
 
 __author__ = "ping"
 __url__ = "https://github.com/ping/odmpy/"
-__version__ = "0.7.1"  # also update odmpy/odm.py
+__version__ = "0.7.2"  # also update odmpy/odm.py
 
 
 __long_description__ = """
