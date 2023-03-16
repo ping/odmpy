6fd15104eb248f0e7475ff4485bddcbfcc0a54d7
ping <ping@users.noreply.github.com>
2023-03-16 11:18:14 +0800

    Fix cov2md to use pathlib as well

--- cov2md.py
+++ cov2md.py
@@ -1,5 +1,5 @@
 import json
-import os
+from pathlib import Path
 
 #
 # Simple script to convert coverage.json into markdown for display as GitHub Actions job summary
@@ -11,7 +11,9 @@ def _escape(txt: str) -> str:
 
 
 def convert(cover_json_filepath, markdown_filepath):
-    with open(cover_json_filepath, "r", encoding="utf-8") as f:
+    cover_json_path = Path(cover_json_filepath)
+    markdown_path = Path(markdown_filepath)
+    with cover_json_path.open("r", encoding="utf-8") as f:
         results = json.load(f)
 
     job_summary = ""
@@ -23,10 +25,10 @@ def convert(cover_json_filepath, markdown_filepath):
         summary = v.get("summary", {})
         job_summary += f'| {_escape(k)} | {summary.get("num_statements", 0)} | {summary.get("missing_lines", 0)} | {summary.get("percent_covered", 0):.1f}% |\n'
 
-    with open(markdown_filepath, "w", encoding="utf-8") as f:
+    with markdown_path.open("w", encoding="utf-8") as f:
         f.write(job_summary)
     try:
-        os.remove(cover_json_filepath)
+        cover_json_path.unlink()
     except:  # noqa: E722, pylint: disable=bare-except
         pass
 
