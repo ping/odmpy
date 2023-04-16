f38932998c73ec716cccb91c31e5e0b71c0dfb8d
ping <ping@users.noreply.github.com>
2023-04-16 21:41:14 +0800

    Improve libby tests

--- tests/odmpy_libby_tests.py
+++ tests/odmpy_libby_tests.py
@@ -783,6 +783,13 @@ class OdmpyLibbyTests(BaseTestCase):
         self.assertTrue(
             self.test_downloads_dir.joinpath(test_folder, "test-audiobook.opf").exists()
         )
+
+        # book only has 1 part
+        with self.test_data_dir.joinpath("audiobook", "openbook.json").open(
+            "r", encoding="utf-8"
+        ) as o:
+            markers = [toc["title"] for toc in json.load(o)["nav"]["toc"]]
+
         for part_file in part_files:
             mutagen_audio = MP3(part_file, ID3=ID3)
             self.assertTrue(mutagen_audio.tags["CTOC:toc"])
@@ -792,6 +799,7 @@ class OdmpyLibbyTests(BaseTestCase):
             ):
                 self.assertEqual(chap_id, f"ch{i:02d}")
                 chapter = mutagen_audio.tags[f"CHAP:{chap_id}"]
+                self.assertEqual(chapter.sub_frames["TIT2"].text[0], markers[i])
                 if i > 0:
                     prev_chapter = mutagen_audio.tags[
                         f'CHAP:{mutagen_audio.tags["CTOC:toc"].child_element_ids[i - 1]}'
@@ -837,16 +845,26 @@ class OdmpyLibbyTests(BaseTestCase):
         )
         mp3_filepath = self.test_downloads_dir.joinpath(test_folder, "ebook.mp3")
         self.assertTrue(mp3_filepath.exists())
+
+        # book only has 1 part
+        with self.test_data_dir.joinpath("audiobook", "openbook.json").open(
+            "r", encoding="utf-8"
+        ) as o:
+            markers = [toc["title"] for toc in json.load(o)["nav"]["toc"]]
+
         mutagen_audio = MP3(mp3_filepath, ID3=ID3)
         self.assertTrue(mutagen_audio.tags["CTOC:toc"])
         chapters = [t for t in mutagen_audio.tags.getall("CHAP")]
-        for i, tag in enumerate(chapters):
+        self.assertEqual(len(markers), len(chapters))
+
+        for i, chapter in enumerate(chapters):
             # check tags are written in time sequence for merged files
             # because ffmpeg conversion from mp3 to m4b bugs out when
             # CHAPs are not written out in time sequence
             # https://github.com/quodlibet/mutagen/issues/506
+            self.assertEqual(chapter.sub_frames["TIT2"].text[0], markers[i])
             if i > 0:
-                self.assertGreater(tag.start_time, chapters[i - 1].start_time)
+                self.assertGreater(chapter.start_time, chapters[i - 1].start_time)
 
     @responses.activate
     def test_mock_libby_download_audiobook_direct_merge_m4b(self):
