377f53a72300acc2d03776a4278e05bc3e46d968
ping <ping@users.noreply.github.com>
2023-04-17 12:34:25 +0800

    Fix test script

--- run_tests.sh
+++ run_tests.sh
@@ -2,50 +2,5 @@ set -e
 
 coverage erase
 
-# test1.odm - book with ascii meta
-# test2.odm - book with non-ascii meta
-# test3.odm - Issue using mutagen, ref #17
-# test4.odm - HTML entities decoding, ref #19
-
-for test_index in '1' '2' '3' '4'
-do
-  export TEST_ODM="test${test_index}.odm"
-  echo "======================= Testing with ${TEST_ODM} ======================="
-
-  # Tests for `odmpy info` command
-  coverage run --append -m unittest -v tests.OdmpyTests.test_info
-  coverage run --append -m unittest -v tests.OdmpyTests.test_info_json
-
-  # Tests for `odmpy dl` command
-  coverage run --append -m unittest -v tests.OdmpyDlTests
-
-  unset TEST_ODM
-done
-echo '======================================================================'
-
-# Misc Tests
-# test fix for #24 cover download fail
-TEST_ODM="test_ref24.odm" coverage run --append -m unittest -v tests.OdmpyTests.test_cover_fail_ref24
-# test for #26 opf generation
-TEST_ODM="test1.odm" coverage run --append -m unittest -v tests.OdmpyTests.test_opf
-# test odm return
-TEST_ODM="test1.odm" coverage run --append -m unittest -v tests.OdmpyTests.test_odm_return
-TEST_ODM="test1.odm" coverage run --append -m unittest -v tests.OdmpyTests.test_odm_return_fail
-TEST_ODM="test1.odm" coverage run --append -m unittest -v tests.OdmpyTests.test_odm_return_error
-
-# Tests for `odmpy libby` command
-coverage run --append -m unittest -v tests.OdmpyLibbyTests
-
-# Tests for odmpy.libby
-coverage run --append -m unittest -v tests.LibbyClientTests
-
-# Test for odmpy.overdrive
-coverage run --append -m unittest -v tests.OverDriveClientTests
-
-# Tests for odmpy.utils
-coverage run --append -m unittest -v tests.UtilsTests
-
-# Tests for odmpy.processing.shared
-coverage run --append -m unittest -v tests.ProcessingSharedTests
-
 coverage run --append -m odmpy --version
+coverage run --append -m unittest -v tests
